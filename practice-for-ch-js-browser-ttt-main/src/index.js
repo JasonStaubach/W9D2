a32ca3214f98c6a5195efa4a6b7a2ef3749016cd
JasonStaubach <jbstaubach@gmail.com>
2023-06-27 12:40:41 -0400

    setting up css/ttt ul

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,9 @@
 // Add your import statements for View and Game here
+import Game from "./game.js";
+import View from  "./ttt-view.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  this.game = new Game()
+  this.view = new View(game, document.querySelector(".container"))
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,8 +1,18 @@
 class View {
   constructor(game, el) {
+    this.game = new Game();
+    // this.grid = Board.makeGrid();
   }
   
   setupBoard() {
+    let grid = document.createElement('ul')
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        let li = document.createElement('li');
+        li.dataset.pos = [i,j];
+        grid.appendChild(li);
+      }
+    }
   }
   
   handleClick(e) {
