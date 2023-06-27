27aa3e05431835f80eeab4217553968343c51332
Manel-Os <oulds.manel@gmail.com>
2023-06-27 16:10:36 -0400

    handleGameOver

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,9 +1,14 @@
 // Add your import statements for View and Game here
-import Game from "./game.js";
+import Game from "/Users/user/Desktop/App-academy-May-2023/W9D2/practice-for-ch-js-browser-ttt-main/ttt_node/game.js";
 import View from  "./ttt-view.js";
 
+
 document.addEventListener("DOMContentLoaded", () => {
+  window.View = View;
+  window.Game = Game;
+
+
   // Your code here
-  this.game = new Game()
-  this.view = new View(game, document.querySelector(".container"))
+  const game = new Game();
+  const view = new View(game, document.querySelector(".ttt"))
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,27 +1,58 @@
 class View {
   constructor(game, el) {
-    this.game = new Game();
+    this.game = game;
     // this.grid = Board.makeGrid();
+    this.el = el;
+    this.setupBoard();
+    this.handleClick();
   }
   
   setupBoard() {
     let grid = document.createElement('ul')
     for(let i = 0; i < 3; i++){
       for(let j = 0; j < 3; j++){
+        // console.log("hey")
         let li = document.createElement('li');
-        li.dataset.pos = [i,j];
+        li.classList.add("unclicked");
+        li.dataset.pos =[i,j];
         grid.appendChild(li);
       }
     }
+
+    this.el.appendChild(grid);
+    
   }
   
   handleClick(e) {
+    this.el.addEventListener("click", (e)=>{
+      // console.log(e.target.dataset.pos[0],e.target.dataset.pos[2]);
+      this.game.playMove([e.target.dataset.pos[0],e.target.dataset.pos[2]]);
+      e.target.classList.remove("unclicked");
+      e.target.classList.add("clicked");
+      e.target.innerText = this.game.currentPlayer;
+      this.handleGameOver(e.target.dataset.pos[0],e.target.dataset.pos[2]);
+    })
   }
 
-  makeMove(square) {
-  }
+  // makeMove(square) {
+
+  // }
   
-  handleGameOver() {
+  handleGameOver(i,j) {
+    // check row 
+    let row = true;
+    let column = true;
+    // let diagonal = true;
+
+    const a = document.querySelectorAll("li"); 
+    const v = a[(i*3+j)];
+    for( let k = 0; k<3 ; k++){
+      if( a[i*3+k] !== v) row = false;
+      
+      if( a[k*3+j] !== v) column = false;
+    }
+
+    if(row || column) console.log("winner")
   }
 }
 
